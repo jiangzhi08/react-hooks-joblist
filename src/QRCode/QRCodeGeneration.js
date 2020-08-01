c630d2acab41a54bdbc956f9a81869b22c6f4702
z j <jiangzhi08@gmail.com>
2020-07-31 22:15:11 -0500

    fix QRcode

--- src/QRCode/QRCodeGeneration.js
+++ src/QRCode/QRCodeGeneration.js
@@ -4,19 +4,23 @@ import { Container } from "react-bootstrap";
 import AddForm from "./AddForm";
 
 export default function QRCodeGeneration() {
-  var canvas = document.getElementById("canvas");
   const generateQR = async (text) => {
     try {
+      let canvas = document.getElementById("canvas");
       await QRCode.toCanvas(canvas, text);
     } catch (err) {
       console.error(err);
     }
   };
 
+  useEffect(() => {
+    generateQR("https://awesome-darwin-99824f.netlify.app/");
+  }, []);
+
   return (
     <Container className="my-4">
-      <canvas id="canvas"></canvas>
       <AddForm generateQR={generateQR} />
+      <canvas id="canvas"></canvas>
     </Container>
   );
 }
